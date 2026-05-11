ab6dae08b5aede704ed85a4c9d93778ff20a2b40
Tevin Campbell <75921907+Troyzhenny@users.noreply.github.com>
2026-05-11 15:31:27 -0400

    Add background support to SelectionCard and update SelectionScreen to utilize it

--- app/components/SelectionScreen.tsx
+++ app/components/SelectionScreen.tsx
@@ -2,14 +2,14 @@ import SelectionCard from "./ui/SelectionCard";
 import aj1 from "@/public/aj1.png";
 import af1 from "@/public/af1.png";
 
-const cardData = [{ image: aj1 }, { image: af1 }];
+const cardData = [{ image: aj1 }, { image: af1, background: "bg-linear-to-br from-amber-200 to-amber-800" }];
 
 export default function SelectionScreen() {
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
       <div className="flex w-full h-full items-center justify-center gap-28">
         {cardData.map((card, i) => (
-          <SelectionCard key={i} image={card.image} />
+          <SelectionCard key={i} image={card.image} background={card.background} />
         ))}
       </div>
       <div className="flex flex-col items-center w-full pb-5">
--- app/components/ui/SelectionCard.tsx
+++ app/components/ui/SelectionCard.tsx
@@ -2,12 +2,13 @@ import NextImage, { StaticImageData } from "next/image";
 
 interface SelectionCardProps {
   image: StaticImageData | string;
+  background?: string;
 }
 
-export default function SelectionCard({ image }: SelectionCardProps) {
+export default function SelectionCard({ image, background = "bg-linear-to-br from-sky-200 to-sky-800" }: SelectionCardProps) {
   return (
     <div className="relative w-80 perspective-dramatic group cursor-pointer">
-      <div className="w-80 h-96 rounded-lg shadow-md bg-linear-to-br from-sky-200 to-sky-800 relative overflow-hidden group-hover:rotate-y-3 transition-all ease-[cubic-bezier(0.34,1.56,0.64,1)] duration-300">
+      <div className={`w-80 h-96 rounded-lg shadow-md ${background} relative overflow-hidden group-hover:rotate-y-3 transition-all ease-[cubic-bezier(0.34,1.56,0.64,1)] duration-300`}>
       </div>
 
       <NextImage
